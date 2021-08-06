8a705e8626b90dcfeb63a45f8c0e954dc8ddc023
Relari <renzolavador@gmail.com>
2021-08-05 22:32:41 -0500

    refactor: updated project

---
+++ src/main/java/com/renzo/exercises/lessons/java/example/test/MacAddress.java
@@ -0,0 +1,33 @@
+package com.renzo.exercises.lessons.java.example.test;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+
+public class MacAddress {
+
+    public String conseguirMAC(){
+        StringBuilder sb = new StringBuilder();
+        NetworkInterface a;
+        try {
+            a = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+            byte[] mac = a.getHardwareAddress();
+
+
+            for (int i = 0; i < mac.length; i++) {
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ""+sb.toString();
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        MacAddress p = new MacAddress();
+        System.out.println(p.conseguirMAC());
+    }
+}
